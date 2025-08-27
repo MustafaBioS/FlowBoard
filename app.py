d84ffd4a55d78431d022ddddf229e93bcc2e8162
MustafaBioS <bioshpere.voices@gmail.com>
2025-08-28 02:13:05 +0300

    finished the task manager, added an add tasks page and a button next to the added tasks to delete them, i will now begin on the notes part

--- app.py
+++ app.py
@@ -24,6 +24,9 @@ def load_user(uid):
 # MODELS
 
 class Users(db.Model, UserMixin):
+
+    __tablename__ = 'Users'
+
     uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(64), unique=True)
     password = db.Column(db.String(128), nullable=False)
@@ -35,7 +38,8 @@ class Tasks(db.Model):
     tid = db.Column(db.Integer, primary_key = True, autoincrement=True)
     tcontent = db.Column(db.String(64), nullable=False)
     tdate_created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
-    
+    user_id = db.Column(db.Integer, db.ForeignKey('Users.uid'), nullable=False)
+
 class Notes(db.Model):
     nid = db.Column(db.Integer, primary_key=True, autoincrement=True)
     ntitle = db.Column(db.String(64), nullable=False)
@@ -61,7 +65,7 @@ def main():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        return render_template('login/login.html')
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
@@ -80,25 +84,25 @@ def login():
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == 'GET':
-        return render_template('signup.html')
+        return render_template('login/signup.html')
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
 
-        # try:
-        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        try:
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
-        user = Users(username=username, password=hashed_password)
+            user = Users(username=username, password=hashed_password)
 
-        db.session.add(user)
-        db.session.commit()
-        flash("Account Created Successfully", "flash")
-        return redirect(url_for('login'))
+            db.session.add(user)
+            db.session.commit()
+            flash("Account Created Successfully", "flash")
+            return redirect(url_for('login'))
 
-        # except sqlalchemy.exc.IntegrityError: 
-        #     db.session.rollback()
-        #     flash("Username Already Taken", 'flash')
-        #     return redirect(url_for('signup'))
+        except sqlalchemy.exc.IntegrityError: 
+            db.session.rollback()
+            flash("Username Already Taken", 'flash')
+            return redirect(url_for('signup'))
     
     else:
         flash('Signup Failed', 'flash')
@@ -112,22 +116,57 @@ def logout():
     print('working')
     return redirect(url_for('main'))
 
-@app.route('/tasks')
+@app.route('/tasks', methods=['POST', 'GET'])
 @login_required
 def tasks():
-    pass
+    tasks = Tasks.query.filter_by(user_id=current_user.uid).all()
+    return render_template('tasks/tasks.html', tasks=tasks)
+    
 
+
+    
+@app.route('/tasks/add', methods=['POST', 'GET'])
+@login_required
+def addtask():
+    if request.method == 'GET':
+        return render_template('tasks/addtask.html')
+    
+    if request.method == 'POST':
+
+        task = request.form.get('task')
+
+        newtask = Tasks(tcontent=task, user_id=current_user.uid)
+
+        db.session.add(newtask)
+        db.session.commit()
+
+        flash('Task Added Successfully', 'flash')
+        return redirect(url_for("tasks"))
+    
+@app.route('/tasks/delete/<int:task_id>', methods=['POST'])
+def deletetask(task_id):
+    
+    task = Tasks.query.get_or_404(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+    flash("Task Deleted Successfully", 'flash')
+    return redirect(url_for('tasks'))
 
 @app.route('/notes')
 @login_required
 def notes():
-    pass
+    return render_template('notes/notes.html')
 
 
 @app.route('/bookmarks')
 @login_required
 def bookmarks():
     pass
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect(url_for('login'))
 
 # Run
 
